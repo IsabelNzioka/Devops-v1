556078a55c1447e3166c6d735f0cc2d2cb6f4a89
IsabelNzioka <isabelnzioka@gmail.com>
2023-10-26 17:01:50 +0300

    test

---
+++ test_deleteshfiles.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+
+@pytest.fixture
+def run_deleteshfiles_script():
+    os.system("python deleteshfiles.py")
+
+def test_deleteshfiles(run_deleteshfiles_script):
+    found_unexpected_files = False  # A flag to track if any unexpected files are found
+
+    for root, _, files in os.walk("."):
+        for file in files:
+            if file.endswith(".sh"):
+                assert not os.path.exists(os.path.join(root, file)), f"File {file} was not deleted"
+                found_unexpected_files = True
+
+    if not found_unexpected_files:
+        print("Test successful: All .sh files were deleted.")
+
+if __name__ == "__main__":
+    pytest.main([__file__])
